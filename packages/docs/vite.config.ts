7305730d1e2d69c7df5e7a8017d888b7afe87fad
brunosilvainteli <bruno.silva@inteli.edu.br>
2025-01-06 19:28:34 -0300

    ci: remove jekyll

--- packages/docs/vite.config.ts
+++ packages/docs/vite.config.ts
@@ -4,8 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/design-system-ignite/', // Asegure-se de que esse caminho está correto
-  build: {
-    outDir: 'storybook-static', // O diretório correto para saída
-  },
 })
